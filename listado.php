2d09927c13c7f3fd2d174060033e91039db3c927
rafamb <rmonber2302@g.educaand.es>
2022-10-13 10:00:12 +0200

    Comienzo a definir areas para el listado

--- listado.php
+++ listado.php
@@ -1,24 +1,31 @@
 <?php
- 
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>listado</title>
+    <link rel="stylesheet" href="estilosListado/estilos.css">
 </head>
+
 <body>
-    <div class="titulo">
-        <h1>LISTADO</h1>
+    <div class="contenedor">
+        <div class="titulo">
+            <h1>LISTADO</h1>
+        </div>
+        <div class="btnNuevo">
+            <a href="">NUEVO</a>
+        </div>
+        <div class="listado">
+    <h4>holaaaaaaaaaaaaaaaaaaaaaaaaaaa</h4>
+        </div>
     </div>
-    <div class="btnNuevo">
-        <a href=""></a>
-    </div>
-    <div class="listado">
-        
-    </div>
+
 </body>
+
 </html>
