1cf6beff33c0f49a6fa2be062d469c9f512db915
rafamb <rmonber2302@g.educaand.es>
2022-10-13 22:58:13 +0200

    Creo metodo que pinta el listado y lo pruebo

--- listado.php
+++ listado.php
@@ -1,5 +1,24 @@
 <?php
+$array[0][0]=1;
+$array[0][1]="aceite";
+$array[0][2]="imagenAceite";
 
+$array[1][0]=2;
+$array[1][1]="pan";
+$array[1][2]="imagenPan";
+
+function pintaListado($array)
+{
+    for ($i = 0; $i < count($array); $i++) {
+        echo "<tr>
+        <td>" . $array[$i][0] . "</td>
+        <td>" . $array[$i][1] . "</td>
+        <td>" . $array[$i][2] . "</td>
+        <td><a href=''><img src='imagesListado/editar.png' width='30' height='30'></a></td>
+        <td><a href=''><img src='imagesListado/eliminar.png' width='30' height='30'></a></td>
+    </tr>";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -22,7 +41,20 @@
             <a href="">NUEVO</a>
         </div>
         <div class="listado">
-    <h4>holaaaaaaaaaaaaaaaaaaaaaaaaaaa</h4>
+            <table>
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>NOMBRE</td>
+                        <td>IMAGEN</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+    <?php pintaListado($array);?>
+                </tbody>
+            </table>
         </div>
     </div>
 
