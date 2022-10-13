9c079f634daa2711e5eb9d51098ad0c37bd96e61
rafamb <rmonber2302@g.educaand.es>
2022-10-13 09:48:55 +0200

    Commit inical, creaccion de listado

---
+++ listado.php
@@ -0,0 +1,24 @@
+<?php
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>listado</title>
+</head>
+<body>
+    <div class="titulo">
+        <h1>LISTADO</h1>
+    </div>
+    <div class="btnNuevo">
+        <a href=""></a>
+    </div>
+    <div class="listado">
+        
+    </div>
+</body>
+</html>
